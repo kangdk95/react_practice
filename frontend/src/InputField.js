756f7e7509ed3899c5de298ba2dd353869893d03
kdk <kang918332@gmail.com>
2021-03-09 15:26:15 +0900

    Add first version

---
+++ frontend/src/InputField.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class InputField extends React.Component {
+
+  render( ){
+    return (
+      <div className="inputField">
+          asdf
+      </div>
+    );
+  }
+  
+}
+
+export default InputField;
+ 
---
+++ frontend/src/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+
+  render( ){
+    return (
+      <div className="loginForm">
+          asdf
+      </div>
+    );
+  }
+  
+}
+
+export default LoginForm;
+ 
